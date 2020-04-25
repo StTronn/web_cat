c35a7c5f82ac6fe74054d9c184b87ec6741a25b9
rishav <rishav.21m@gmail.com>
2020-04-26 03:56:03 +0530

    minor changes

--- src/components/SimSitesCard.js
+++ src/components/SimSitesCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ({ websites }) {
   return (
-    <div className="min-w-full max-w-sm rounded overflow-hidden shadow-lg ">
+    <div className="inline-block min-w-full max-w-md rounded overflow-hidden shadow-lg ">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2 text-center">Simillar Sites</div>
       </div>

7ee0ddf1746d6fca6262366d0915a691472530d4
rishav <rishav.21m@gmail.com>
2020-04-28 16:30:35 +0530

    Updates

---
+++ precache-manifest.0278e228ae4abdadf04f13b70011caf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b878737f610f7d2a6c3d9adb618534c5",
+    "url": "/web_cat/index.html"
+  },
+  {
+    "revision": "37d7f85ee8b69422fa00",
+    "url": "/web_cat/static/css/main.b4cedb41.chunk.css"
+  },
+  {
+    "revision": "de1ca8454838bba08f33",
+    "url": "/web_cat/static/js/2.7c70309c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web_cat/static/js/2.7c70309c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d7f85ee8b69422fa00",
+    "url": "/web_cat/static/js/main.98c29ded.chunk.js"
+  },
+  {
+    "revision": "f9c4d31fca19af2af20c",
+    "url": "/web_cat/static/js/runtime-main.9a4679ae.js"
+  }
+]);

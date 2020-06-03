7eb85a1817ad09e24f0142fc861c6ba2604f50e6
rishav <rishav.21m@gmail.com>
2020-06-03 10:28:42 +0530

    added build

---
+++ build/precache-manifest.014d6cbb159d8f9dea72fbaf933c67f5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e5e38958c591e8664b3ebd8481fd32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5fe10e0628886eb6726",
+    "url": "/static/css/2.29bdd560.chunk.css"
+  },
+  {
+    "revision": "af901490984d8d85c2d1",
+    "url": "/static/css/main.e1c79258.chunk.css"
+  },
+  {
+    "revision": "a5fe10e0628886eb6726",
+    "url": "/static/js/2.2e0bf11a.chunk.js"
+  },
+  {
+    "revision": "81b6e0f34fc5a80a94fad82949fb96e7",
+    "url": "/static/js/2.2e0bf11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af901490984d8d85c2d1",
+    "url": "/static/js/main.455c5433.chunk.js"
+  },
+  {
+    "revision": "b72fb7a54e3bbb40b7ee",
+    "url": "/static/js/runtime-main.144ff9f2.js"
+  },
+  {
+    "revision": "55cca5267f68f9ce1ad3b8d4a8090a58",
+    "url": "/static/media/Feedback-01.55cca526.svg"
+  },
+  {
+    "revision": "485b0dbc00ab2261dd3a73a25434ceea",
+    "url": "/static/media/cat_info2.485b0dbc.png"
+  },
+  {
+    "revision": "9a6fc0101494ae12862c713721e3dfc1",
+    "url": "/static/media/web_cat_landing1.9a6fc010.jpg"
+  }
+]);
